9afdee900dda266acdbcd0f41bcc471d2b88813b
kushwahawakeel12 <wakeelkushwaha1212@gmail.com>
2025-07-09 18:16:50 +0530

    complete food recipe projct

--- Food_Recipe/src/Componenst/Meal.jsx
+++ Food_Recipe/src/Componenst/Meal.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
+import './Meal.css'
 
 export const Meal = () => {
 
   const [mealData, setMealData] = useState([])
+  const [area, setArea] = useState('Indian')
+  const [inputData, setInputData] = useState('')
 
   useEffect(() => {
 
     const fetchDataFromApi = async () => {
-      const Api = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian`)
+      const Api = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
       const data = await Api.json()
       console.log("api data = ", data)
 
@@ -16,57 +19,53 @@ export const Meal = () => {
     }
     fetchDataFromApi()
 
-  }, [])
+  }, [area])
+
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    const Api = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputData}`)
+    const data = await Api.json()
+
+    console.log("search by  = ", data.meals)
+    setMealData(data.meals)
+    setInputData("")
+  }
 
   return (
- <>
- {/* <div className='max-auto text-center margin-10'>
-
- <button type="button" class="btn btn-outline-primary max-3">Primary</button>
-
-  
-<button type="button" class="btn btn-outline-secondary">Secondary</button>
-
-
-
-<button type="button" class="btn btn-outline-success">Success</button>
-
-
-
-<button type="button" class="btn btn-outline-danger">Danger</button>
+    <>
 
 
 
 
-<button type="button" class="btn btn-outline-warning">Warning</button>
 
+      <div className='mx-auto text-center'>
+        <form onSubmit={submitHandler} className='mx-auto text-center my-3 '>
+          <input onChange={(e) => setInputData(e.target.value)} type="text" value={inputData} />
+        </form>
+        <button onClick={() => setArea('Indian')} type='button' className='btn btn-outline-primary mx-3' >Indian</button>
 
-<button type="button" class="btn btn-outline-info">Info</button>
+        <button onClick={() => setArea('American')} type='button' className='btn btn-outline-warning mx-3'
+        >American</button>
 
-</div> */}
+        <button onClick={() => setArea("Thai")} type='button' className='btn btn-outline-light mx-3'>Thai</button>
+        <button onClick={() => setArea("Canadian")} type='button' className='btn btn-outline-info mx-3'>Canadian</button>
+        <button onClick={() => setArea("British")} type='button' className='btn btn-outline-danger mx-3'>British</button>
+        <button onClick={() => setArea('Russian')} type='button' className='btn btn-outline-success mx-3'>Russian</button>
 
+      </div>
 
-
-
-<div className='mx-auto text-center'>
-  <button type='button' className='btn btn-outline-primary mx-3'>Indian</button>
-
-  <button type='button' className='btn btn-outline-primary mx-3'>American</button>
-  <button type='button' className='btn btn-outline-primary mx-3'>Thai</button>
-  <button type='button' className='btn btn-outline-primary mx-3'>Canadian</button>
-</div>
-
-      <div style={{display:"flex" ,justifyContent:'center',alignItems:'center',flexWrap:'wrap' ,gap:'60px'}}>
+      <div style={{ display: "flex", justifyContent: 'center', alignItems: 'center', flexWrap: 'wrap', gap: '60px', margin: '30px' }}>
         {
 
-          mealData.map((meal) => <div key={meal.idMeal}style={{textAlign:'center'}}  >
+          mealData.map((meal) => <div key={meal.idMeal} style={{ textAlign: 'center' }}  >
             <div>
-            <img src={meal.strMealThumb} alt={meal.strMeal} style={{width:"220px",border:"2px solid blue",borderRadius:"10px"}} />
-          </div>
+              <img src={meal.strMealThumb} alt={meal.strMeal} style={{ width: "220px", border: "2px solid blue", borderRadius: "10px" }} />
+            </div>
             <h5>{meal.strMeal}</h5>
           </div>)
         }
       </div>
- </>
+    </>
   )
 }
